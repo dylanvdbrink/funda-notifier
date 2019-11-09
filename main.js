a4d2010896ae6b1e304be177be69dea3e1ea8889
dylanvdbrink <dylanvandenbrink@live.nl>
2019-11-09 14:47:44 +0100

    Added the ability to configure the storage and config paths

--- main.js
+++ main.js
@@ -4,22 +4,6 @@ const pushover = require('pushover-notifications');
 const log4js = require('log4js');
 const YAML = require('yaml');
 
-// Get config
-const configFile = fs.readFileSync('./config.yml', 'utf8')
-const config = YAML.parse(configFile);
-
-for (let a in config) {
-    let query = config[a];
-    notifyForFundaListings(a, query.type, query.city, query.minPrice, query.maxPrice, query.maxRange);
-}
-
-
-// Configure pushover
-var p = new pushover({
-    user: process.env['PUSHOVER_USER'],
-    token: process.env['PUSHOVER_TOKEN']
-});
-
 // Configure log4js
 log4js.configure({
     appenders: { 
@@ -30,20 +14,47 @@ log4js.configure({
 });
 const logger = log4js.getLogger();
 
+// Storage location
+var storagePath = process.argv[3];
+logger.info('storagePath: ' + storagePath);
+if (!fs.existsSync(storagePath)) {
+    throw new Error(storagePath + ' is not a valid location for the storage location');
+}
+
+// Get config
+var configPath = process.argv[2];
+logger.info('configPath: ' + configPath);
+if (!fs.existsSync(configPath)) {
+    throw new Error(configPath + ' is not a valid location for the config file');
+}
+
+const configFile = fs.readFileSync(configPath, 'utf8')
+const config = YAML.parse(configFile);
+
+for (let a in config) {
+    let query = config[a];
+    notifyForFundaListings(a, query.type, query.city, query.minPrice, query.maxPrice, query.maxRange);
+}
+
+// Configure pushover
+var p = new pushover({
+    user: process.env['PUSHOVER_USER'],
+    token: process.env['PUSHOVER_TOKEN']
+});
+
 function notifyForFundaListings (title, type, city, minPrice, maxPrice, maxRange) {
     feed.load(`http://partnerapi.funda.nl/feeds/Aanbod.svc/rss/?type=${type}&zo=/${city}/${minPrice}-${maxPrice}/+${maxRange}/sorteer-datum-af/`, function(err, rss) {   
         if (!err) {
             let items = rss.items;
-            let dir = './data/';
             let idregex = /([0-9]{5,})/g;
 
-            if (items.length > 0 && !fs.existsSync(dir)){
-                fs.mkdirSync(dir);
+            if (items.length > 0 && !fs.existsSync(storagePath)){
+                fs.mkdirSync(storagePath);
             }
 
             for (let item of items) {
                 let filename = item.url.match(idregex) + '.json';
-                if (!fs.existsSync(dir + filename)) {
+                if (!fs.existsSync(storagePath + filename)) {
                     let amountRegex = /(\d+\.\d+)/g;
                     let spaceRegex = /\-\ (.*)/g;
                     var space = item.description.match(spaceRegex)[0];
@@ -62,7 +73,8 @@ function notifyForFundaListings (title, type, city, minPrice, maxPrice, maxRange
                             logger.info('Succesfully sent pushover request for ' + filename);
                         }
                     });
-                    //fs.writeFileSync(dir + filename, JSON.stringify(item));
+                    fs.writeFileSync(storagePath + filename, JSON.stringify(item));
+                    logger.info('Created ' + storagePath + filename);
                 } else {
                     logger.info(filename + ' already exists.');
                 }
